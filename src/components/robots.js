765e02cd97a11850e605231e62a3082485ca86bb
nicokest <nicolask10@gmail.com>
2019-12-22 21:35:12 -0300

    version final

---
+++ src/components/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leon Kestelboim',
+        username: 'perrogamer360',
+        email: 'perrogamer360@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Diego de los Palotes',
+        username: 'Palotes',
+        email: 'JPalotes@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Juana de Arco',
+        username: 'Arco',
+        email: 'juanadearco@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Segundo Domingo Sombra',
+        username: 'Sombra',
+        email: 'dsombra@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Dolores Claymore',
+        username: 'Claymore',
+        email: 'doloclay@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Rodrigo Distefano',
+        username: 'Distefano',
+        email: 'rododistefano@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Ricardo Carlos Piro',
+        username: 'Prio',
+        email: 'juancapiro@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Paula San Juan',
+        username: 'Santa',
+        email: 'pausanjuan@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Romina Parla',
+        username: 'Sana',
+        email: 'rominasana@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Tony Benavidez',
+        username: 'Bena',
+        email: 'eltonybenavidez@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Ana de los Palotes',
+        username: 'AnaPalo',
+        email: 'anapalo@gmail.com'
+    },
+]
